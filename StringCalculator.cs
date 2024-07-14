3484ea7e8bec97b5b0d0a668874f498dfb2e3579
Omkar Gurav <64888669+OmkarGurav6@users.noreply.github.com>
2024-07-14 21:22:18 +0530

    Testing 3rd case

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -8,6 +8,26 @@ public class StringCalculator
     {
        if(string.IsNullOrEmpty(numbers) || (numbers == "0"))
            return 0;
-       return -1;
+
+       string[] delimiters = GetDelimiters(numbers);
+       List<int> numberList = GetNumbers(numbers, delimiters);
+       return GetSum(numbersList);
+    }
+
+    
+    private string[] GetDelimiters(string numbers)
+    {
+        string[] delimiters = new string[] {","};        
+        return delimiters;
+    }
+
+     private List<int> GetNumbers(string numbers, string[] delimiters)
+    {
+        return numbers.Split(delimiters).Select(int.Parse).ToList();
+    }
+
+    private int GetSum(List<int> numbers)
+    {
+        return numbers.Where(n => n <= 1000).Sum();
     }
 }
