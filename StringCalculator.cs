2e84811d6ad3f9df8e8d7b2c30b13bdbdf8757c9
Omkar Gurav <64888669+OmkarGurav6@users.noreply.github.com>
2024-07-14 20:40:12 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,1 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers))
+           return 1;
+    }
+}
